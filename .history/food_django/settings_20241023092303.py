d08db13d4461ea0e731168e0407086c6fa062407
masamichan <positivemasa031@gmail.com>
2024-10-23 12:23:39 +0900

    Database & Models

--- .history/food_django/settings_20241023092303.py
+++ .history/food_django/settings_20241023092303.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'food.apps.FoodConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
